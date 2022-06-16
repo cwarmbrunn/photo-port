0c2cda0be3186596f7dbd70160d7611487f76565
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-06-16 17:47:45 -0400

    added and confirmed test ran successfully

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -6,17 +6,34 @@ import "@testing-library/jest-dom/extend-expect";
 
 import Nav from "..";
 
+const categories = [
+  { name: "portraits", description: "Portraits of people in my life" },
+];
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
 afterEach(cleanup);
 
 describe("Nav component", () => {
   // Baseline test
 
   it("renders", () => {
-    render(<Nav />);
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
   });
   // Snapshot test
   it("matches snapshot", () => {
-    const { asFragment } = render(<Nav />);
+    const { asFragment } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
     // Assert value comparison
     expect(asFragment()).toMatchSnapshot();
   });
@@ -25,7 +42,13 @@ describe("Nav component", () => {
 describe("Emoji is visible", () => {
   it("inserts emoji into the h2", () => {
     // Arrange
-    const { getByLabelText } = render(<Nav />);
+    const { getByLabelText } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
     // Assert
     expect(getByLabelText("camera")).toHaveTextContent("📸");
   });
@@ -34,7 +57,13 @@ describe("Emoji is visible", () => {
 describe("Links are visible", () => {
   it("insert text into the links", () => {
     // Arrange
-    const { getByTestId } = render(<Nav />);
+    const { getByTestId } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
     expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
     expect(getByTestId("about")).toHaveTextContent("About me");
     // Assert
