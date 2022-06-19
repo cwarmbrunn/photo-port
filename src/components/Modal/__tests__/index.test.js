5f98967cc46373f7545d4b391529837805f168d4
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-06-19 17:22:41 -0400

    adjustments

--- src/components/Modal/__tests__/index.test.js
+++ src/components/Modal/__tests__/index.test.js
@@ -1,12 +1,15 @@
+// __tests__/Modal.test.js with hard coded categories
 import React from "react";
-import { render, cleanup, fireEvent, getByText } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Modal from "..";
 
+const mockToggleModal = jest.fn();
 const currentPhoto = {
   name: "Park bench",
   category: "landscape",
-  description: "Lorem ipsum",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
   index: 1,
 };
 
@@ -14,26 +17,25 @@ afterEach(cleanup);
 
 describe("Modal component", () => {
   it("renders", () => {
-    // Baseline render component test
+    render(<Modal onClose={mockToggleModal} currentPhoto={currentPhoto} />);
   });
-  // Snapshot test
-});
 
-it("matches snapshot DOM node structure", () => {
-  // Arrange the snapshot - declare variables
-  // Assert the match
+  it("matches snapshot DOM node structure", () => {
+    const { asFragment } = render(
+      <Modal onClose={mockToggleModal} currentPhoto={currentPhoto} />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
 
 describe("Click Event", () => {
-  // Arrange: Render Modal
-  // Act: Simulate Click Event
-  // Assert: Expected matcher
+  it("calls onClose handler", () => {
+    const { getByText } = render(
+      <Modal onClose={mockToggleModal} currentPhoto={currentPhoto} />
+    );
+    fireEvent.click(getByText("Close this modal"));
 
-  const { getByText } = render(
-    <Modal onClose={mockToggleModal} currentPhoto={currentPhoto} />
-  );
+    expect(mockToggleModal).toHaveBeenCalledTimes(1);
+  });
 });
-
-fireEvent.click(getByText("Close this modal"));
-
-expect(mockToggleModal).toHaveBeenCalledTimes(1);
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 
-function Modal({ currentPhoto }) {
-  const { name, category, description, index } = currentPhoto;
+const Modal = ({ onClose, currentPhoto }) => {
+  const { name, description, category, index } = currentPhoto;
+
   return (
     <div className="modalBackdrop">
       <div className="modalContainer">
-        <h3 className="modalTitle">Photo Name</h3>
-        <img alt="current category" />
-        <p>Photo Description</p>
-        <button type="button">Close this modal</button>
+        <h3 className="modalTitle">{name} </h3>
+        <img
+          src={require(`../../assets/large/${category}/${index}.jpg`).default}
+          alt="current category"
+        />
+        <p>{description}</p>
+        <button type="button" onClick={onClose}>
+          Close this modal
+        </button>
       </div>
     </div>
   );
-}
+};
 
-// Export
 export default Modal;
